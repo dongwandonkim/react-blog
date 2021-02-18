60f29950de2fb3d86d171e169611bb8b81762ce5
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-02-17 22:35:46 -0800

    useState tutorial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [postTitle, setPostTitleState] = useState([
+    'men jacket',
+    'good jean',
+    'fancy shirt',
+  ]);
+  const [likeCount, setLikeCount] = useState(0);
+
+  function changeTitle() {
+    let newTitleArray = [...postTitle];
+    newTitleArray[0] = 'women coat';
+    setPostTitleState(newTitleArray);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hi</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">Dongwan Kim</div>
+      <div className="section">
+        <div className="posts">
+          <h3>
+            {postTitle[0]}
+            <span
+              onClick={() => {
+                setLikeCount(likeCount + 1);
+              }}
+              style={{ cursor: 'pointer' }}
+            >
+              👍
+            </span>
+            {likeCount}
+          </h3>
+          <p>내용</p>
+          <p>Date</p>
+          <button onClick={changeTitle}>click</button>
+          <div className="border"></div>
+        </div>
+        <div className="posts">
+          <h3>{postTitle[1]}</h3>
+          <p>내용</p>
+          <p>Date</p>
+          <div className="border"></div>
+        </div>
+        <div className="posts">
+          <h3>{postTitle[2]}</h3>
+          <p>내용</p>
+          <p>Date</p>
+          <div className="border"></div>
+        </div>
+      </div>
     </div>
   );
 }
